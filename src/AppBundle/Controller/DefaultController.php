ac71ebeade47d0ba6240f9ac3d85920c16953b56
Pakoo <dpakoo906@gmail.com>
2016-04-29 12:46:07 +0200

    Dodanie kolejnych podstron + szkielet backendu

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -30,4 +30,37 @@ class DefaultController extends Controller
     {
         return $this->render('portfolio/portfolio.html.twig');
     }
+    
+    /**
+     * @Route("/work/{id}", name="work", defaults={"id" = null})
+     */
+    public function workAction($id)
+    {
+        if ($id) {
+            //logika pobrania i wyświetlenia konkretnej pracy z bazy
+        } else {
+            return $this->redirectToRoute('homepage');
+        }
+    }
+    
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blogAction()
+    {
+        return $this->render('/portfolio/blog.html.twig');
+    }
+    
+    /**
+     * @Route("/post/{title}", name="post", defaults={"title" = null})
+     */
+    public function postAction($title)
+    {
+        if ($title) {
+            //logika wyciągania posta z bazy 
+        } else {
+            return $this->redirectToRoute('homepage');
+            //lub wywal 404 -> jeszcze do przemyślenia
+        }
+    }
 }
